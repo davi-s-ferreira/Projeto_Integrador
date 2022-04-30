cb85100e985c07e3ca0cbce23f5abda9378e05c9
ErikaGusmao <97561655+davi-s-ferreira@users.noreply.github.com>
2022-04-29 21:27:05 -0300

    commit davi

---
+++ cadastro.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Gestão de usuários - Cadastro</title>
+</head>
+<body>
+<form>
+            <h2>Cadastre-se</h2>
+            <hr>
+            <p><strong>Cadastre-se para termos acesso as suas informações e entrarmos em contato.</p>
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Endereço de email:</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Digite seu email...">
+    <div id="emailHelp" class="form-text">Nunca compartilharemos seu e-mail com mais ninguém.</div>
+  </div>
+  
+  <div class="mb-3">
+    <label for="inputPassword4" class="form-label">Senha:</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Digite sua senha...">
+    <div id="emailHelp" class="form-text">Deve ter de 8 a 20 caracteres.</div>
+  </div>
+  <div class="mb-3">
+    <label for="inputCity" class="form-label">Cidade:</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Digite sua cidade...">
+  </div>
+    </span>
+  </div>
+</div>
+
+  
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+</body>
+</html>
